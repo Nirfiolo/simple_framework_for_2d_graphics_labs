14250e71e0d1020197bbdee7b7d6be87345d8b8e
Nirfiolo <bodyaufml@gmail.com>
2020-04-06 10:45:37 +0300

    Inited sfml

--- Computer_Graphics_Labs/Framework/main.cpp
+++ Computer_Graphics_Labs/Framework/main.cpp
@@ -1,6 +1,21 @@
+#include "SFML/Graphics.hpp"
 
-
-int main()
+int main(int argc, char ** argv)
 {
-    return 0;
+    sf::RenderWindow render_window(sf::VideoMode(640, 480), "Hello World");
+
+    while (render_window.isOpen())
+    {
+        sf::Event event;
+
+        while (render_window.pollEvent(event))
+        {
+            if (event.type == sf::Event::EventType::Closed)
+            {
+                render_window.close();
+            }
+        }
+        render_window.clear(sf::Color::White);
+        render_window.display();
+    }
 }
